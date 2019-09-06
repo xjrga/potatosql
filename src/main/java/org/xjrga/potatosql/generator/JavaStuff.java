3ac61aeeb49954ef717c310c2647d3147f37031d
Jorge Garcia de Alba <jorge.r.garciadealba@gmail.com>
2019-09-06 11:37:08 -0500

    wip

--- src/main/java/org/xjrga/potatosql/generator/JavaStuff.java
+++ src/main/java/org/xjrga/potatosql/generator/JavaStuff.java
@@ -205,14 +205,108 @@ public class JavaStuff
         int count = table.getCountKey();
         for (int i = 0; i < count; i++)
         {
-            sb.append("row.put(\"");
-            sb.append(table.getColumn(i).getName().toUpperCase());
-            sb.append("\", ");
-            sb.append("rs.getObject(");
-            sb.append(i + 1);
-            sb.append("));");
+            Column column = table.getColumn(i);
+            sb.append("row.put(");
+            sb.append(getParam(i + 1, column));
+            sb.append(");");
             sb.append("\n");
         }
+        return sb.toString();
+    }
+
+    private String getParam(int count, Column column)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        /*
+        *   call keytype_insert(0,'IDENTITY',false,false);
+            call keytype_insert(1,'INTEGER',false,false);
+            call keytype_insert(2,'DOUBLE',false,false);
+            call keytype_insert(3,'VARCHAR',true,false);
+            call keytype_insert(4,'DATE',false,false);
+            call keytype_insert(5,'TIME',false,false);
+            call keytype_insert(6,'TIMESTAMP',false,false);
+            call keytype_insert(7,'DECIMAL',true,true);
+            call keytype_insert(8,'LONGVARCHAR',false,false);
+            call keytype_insert(9,'BLOB',false,false);
+            call keytype_insert(10,'CLOB',false,false);
+        * */
+        switch (column.getTypeName())
+        {
+            case "INTEGER":
+            case "IDENTITY":
+                sb.append("rs.getInt(");
+                sb.append(count);
+                sb.append(",");
+                sb.append(column.getName());
+                sb.append(")");
+                break;
+            case "DOUBLE":
+                sb.append("rs.getDouble(");
+                sb.append(count);
+                sb.append(",");
+                sb.append(column.getName());
+                sb.append(")");
+                break;
+            case "VARCHAR":
+            case "LONGVARCHAR":
+                sb.append("rs.getString(");
+                sb.append(count);
+                sb.append(",");
+                sb.append(column.getName());
+                sb.append(")");
+                break;
+            case "DATE":
+                sb.append("rs.getDate(");
+                sb.append(count);
+                sb.append(",");
+                sb.append(column.getName());
+                sb.append(")");
+                break;
+            case "TIME":
+                sb.append("rs.getTime(");
+                sb.append(count);
+                sb.append(",");
+                sb.append(column.getName());
+                sb.append(")");
+                break;
+            case "TIMESTAMP":
+                sb.append("rs.getTimestamp(");
+                sb.append(count);
+                sb.append(",");
+                sb.append(column.getName());
+                sb.append(")");
+                break;
+            case "DECIMAL":
+                sb.append("rs.getBigDecimal(");
+                sb.append(count);
+                sb.append(",");
+                sb.append(column.getName());
+                sb.append(")");
+                break;
+            case "BLOB":
+                sb.append("rs.getBlob(");
+                sb.append(count);
+                sb.append(",");
+                sb.append(column.getName());
+                sb.append(")");
+                break;
+            case "CLOB":
+                sb.append("rs.getClob(");
+                sb.append(count);
+                sb.append(",");
+                sb.append(column.getName());
+                sb.append(")");
+                break;
+            default:
+                sb.append("rs.getObject(");
+                sb.append(count);
+                sb.append(",");
+                sb.append(column.getName());
+                sb.append(")");
+                break;
+        }
+
         return sb.toString();
     }
 }
