3a98119b6c3b1e6f543eb1cc5746eb9e99f72fa0
Jorge Garcia de Alba <jorge.r.garciadealba@gmail.com>
2019-09-09 10:10:57 -0500

    wip

--- data/sql/3_load_keytypes.sql
+++ data/sql/3_load_keytypes.sql
@@ -1,12 +1,23 @@
 --Insert
 call keytype_insert(0,'IDENTITY',false,false);
+/
 call keytype_insert(1,'INTEGER',false,false);
+/
 call keytype_insert(2,'DOUBLE',false,false);
+/
 call keytype_insert(3,'VARCHAR',true,false);
+/
 call keytype_insert(4,'DATE',false,false);
+/
 call keytype_insert(5,'TIME',false,false);
+/
 call keytype_insert(6,'TIMESTAMP',false,false);
+/
 call keytype_insert(7,'DECIMAL',true,true);
+/
 call keytype_insert(8,'LONGVARCHAR',false,false);
+/
 call keytype_insert(9,'BLOB',false,false);
+/
 call keytype_insert(10,'CLOB',false,false);
+/
--- data/sql/4_load_relationshiptypes.sql
+++ data/sql/4_load_relationshiptypes.sql
@@ -1,2 +1,4 @@
 call relationshiptype_insert(0,'Identifying');
+/
 call relationshiptype_insert(1,'Non-Identifying');
+/
